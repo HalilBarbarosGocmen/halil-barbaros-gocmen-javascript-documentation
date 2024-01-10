8b8b37bcfa32ca48fdfe216e2c90eed4188515fa
Halil Barbaros Gocmen <HalilBarbarosG@gmail.com>
2024-01-10 20:31:57 +0300

    new part added.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+//! Bu kodda yazılanların daha anlaşılır olması için 'Better Comments' eklentisi kullanılmıştır.
+
+
 //! Bu Renkte Yazılan Metinler Ana Konu Başlıklarıdır.
 //* Bu Renkte Yazılan Metinler İç Konu Başlıklarıdır.
 // Bu renkte Yazılan Metinler Açıklamalardır.
@@ -196,3 +199,75 @@
 //? }
 
 // Yukarıdaki örnekte ehliyetimizin olması gerektiğini, iyi bir görüşümüzün olması gerektiğini ve yorgun olmamamız gerektiğini görüyoruz. Bu değerleri sağladığımız içinde arabayı kullanabileceğimizin çıktısını alıyoruz.
+
+//! Switch Case Nedir Ve Nasıl Kullanılır ?
+
+// Switch case if else-if koşullu kontrol operatörünün farklı bir syntax ile yazılmış biçimidir. Koşullarımıza göre ilgili kod bloğu çalışacaktır.
+
+//? const day = 'thursday'
+
+//? switch (day) {
+//?   case 'monday':
+//?        console.log('Plan course structure');
+//?        console.log('Go to coding meetup');
+//?        break;
+//?   case 'thursday':
+//?        console.log('Prepare theory videos');
+//?        break;
+//?   case 'wednesday':
+//?   case 'thursday':
+//?       console.log('Write code examples');
+//?        break;
+//?   case 'friday':
+//?        console.log('Record videos');
+//?        break;
+//?   case 'saturday':
+//?   case 'sunday':
+//?        console.log('Enjoy the weekend');
+//?        break;
+//?   default:
+//?        console.log('Not a valid day')
+//? }
+
+// Yukarıda switch case karşılaştırma operatörü örneği verilmiştir, aşağıda ise if else-if koşullu kontrol operatörü örneği mevcuttur.
+
+//? if (day === 'monday') {
+//?    console.log('Plan course structure')
+//?    console.log('Go to coding meetup');
+//? } else if (day === 'thursday') {
+//?    console.log('Prepare theory videos');
+//? } else if (day === 'wednesday' || 'thursday') {
+//?    console.log('Write code examples');
+//? } else if (day === 'friday') {
+//?    console.log('Record videos');
+//? } else if (day === 'saturday' || day === 'sunday') {
+//?    console.log('Enjoy the weekend');
+//? } else {
+//?    console.log('Not a valid day');
+//? }
+
+//! Koşullu İfadeler (Ternary Operator) Nedir Ve Nasıl Kullanılır ?
+
+// Switch-case ve if-else ifadesine benzer içeriği tek satırda yazmamızı sağlayan koşullu kontrol operatörüdür.
+
+//? const age = 23;
+//? const drink = age >18 ? 'wine' : 'water';
+//? console.log(drink);
+
+// Yukarıda Ternary Operator örneği verilmiştir. '?' ifadesi 'ise' anlamına gelirken ':' ifadesi 'değil ise' anlamına gelmektedir.
+
+//! Strict Mode (Katı Mod) Nedir Ve Nasıl Kullanılır ?
+
+// Strict Mode JavaScript'te yapılan hataları ve değişken isimlerinde hali hazırda JavaScript tarafından kullanılan isimleri kullanmamamız gerektiğini bize consol üzerinden gösteren adeta bir nevi yardımcıdır. Strict Mode'u kullanabilmek için JavaScript kodumuzun başına 'use strict'; yazmamız yeterli olacaktır.
+
+//! Fonksiyonlar (Functions) Nedir Ve Nasıl Kullanılır ?
+
+// En basit haliyle bir fonksiyon, kodumuzda tekrar tekrar kullanabileceğimiz bir kod parçasıdır.
+
+//? function logger() {
+//?    console.log('My name is Barbaros');
+//? }
+
+//? logger();
+
+// Yukarıda ki örnekte logger adında bir fonksiyon oluşturduk ve süslü parantezlerin içerisine çalıştırmasını istediğimiz kod satırını girdik. Alt satırda oluşturduğumuz fonksiyonun adını ve parantezi yazarsak yani 'logger();' yazar isek oluşturduğumu fonsiyonu tekrar tekrar her yazışımızda çağırabilir-çalıştırabiliriz ve böylece her çağırdığımzıda oluşturduğumuz fonksiyonun içerisindeki kod çalışacaktır.
